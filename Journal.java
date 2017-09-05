0707fa5324ab038952f45336e07ec462d095d6f2
David Mulligan <fmu2174304@hotmail.com>
2017-09-05 23:43:53 +0100

    Add an attempt at homework

---
+++ Journal.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.HashMap;
+
+public class HashMapJournal{
+  public static void main(String[] args) {
+
+    HashMap<String,String> Journal = new HashMap<String, String>();
+
+    journal.put("Monday", "Fish");
+    journal.put("Tuesday", "Fish");
+    journal.put("Wednesday", "Fish");
+    journal.put("Thursday", "Fish");
+    journal.put("Friday", "Fish");
+    journal.put("Saturday", "Fish");
+    journal.put("Sunday", "Fish");
+
+    public string fishCount(day){
+      return journal.get(day)
+    }
+
+    public int weeklyCount(){
+      for(String fish: jounral.key() ){
+        salmon = journal.get(fish);
+        return salmon.size();
+      }
+    }
+
+
+  }
---
+++ JournalTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class JournalTest{
+  Journal journal;
+
+
+}
